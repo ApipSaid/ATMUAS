db63c3975ff2e92da44aed7a331b18e568e730a2
ApipSaid <afif.ghufran@students.uag.ac.id>
2024-12-18 20:55:41 -0800

    Tugas ATM UAS

---
+++ src/main/java/com/mycompany/projectatm/ProjectATM.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projectatm;
+
+/**
+ *
+ * @author LAB1803-PC01
+ */
+public class ProjectATM {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
